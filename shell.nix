924398f59eebc6ea0115e3b035f69bb1c52ea840
Shane Vandegrift <vandesm14@gmail.com>
2023-08-03 10:23:19 -0400

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  name = "conet";
+  buildInputs = [
+    # I have this on my system and don't want it installed. You all will have to suffer for right now.
+    # pkgs.google-cloud-sdk
+    pkgs.openssl
+  ];
+
+  shellHook = ''
+    export GCLOUD_BEARER=$(gcloud auth application-default print-access-token)
+  '';
+}
