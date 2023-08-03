dce49e05e9fc28ba80c8e31307cf28b0dfbfe056
Shane Vandegrift <vandesm14@gmail.com>
2023-08-03 10:23:26 -0400

    update shell files

--- shell.nu
+++ shell.nu
@@ -1,2 +1,1 @@
-^mkdir -p /tmp/conet
 let-env GCLOUD_BEARER = (gcloud auth application-default print-access-token)
--- shell.sh
+++ shell.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-
-mkdir -p /tmp/conet
 export GCLOUD_BEARER=$(gcloud auth application-default print-access-token)
