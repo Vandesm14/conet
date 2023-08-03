d8aaf43d16b0b22d93038b41a4f7d1f2941189ad
Shane Vandegrift <vandesm14@gmail.com>
2023-08-03 15:45:51 -0400

    use traits for rendering

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use core::fmt;
-use std::fmt::{Display, Formatter};
+use std::{
+  borrow::BorrowMut,
+  fmt::{Display, Formatter},
+  rc::Rc,
+};
 
+use async_trait::async_trait;
 use conet::Tts;
 use hound::WavSpec;
 use lowpass_filter::lowpass_filter;
 use spellabet::{PhoneticConverter, SpellingAlphabet};
+
+#[async_trait]
+trait Render {
+  async fn render(&self, samples: &mut Vec<f32>, tts: &mut Tts);
+}
 
 enum Encoding {
   Words,
@@ -49,6 +59,86 @@ impl<'a> Speak<'a> {
   }
 }
 
+#[async_trait]
+impl<'a> Render for Speak<'a> {
+  async fn render(&self, samples: &mut Vec<f32>, tts: &mut Tts) {
+    match self.encoding {
+      Some(Encoding::Words) => {
+        // Split the secret phrase into words
+        let words = self.text.split_whitespace();
+
+        // Run through each word and TTS samples
+        for word in words {
+          let more_samples = tts.generate(word, self.model()).await;
+          samples.extend(more_samples);
+        }
+      }
+      Some(Encoding::Ascii) => {
+        // Convert secret phrase into ascii codes (String of numbers)
+        let words = self
+          .text
+          .as_bytes()
+          .iter()
+          // Convert each byte into a string, padded with 0s
+          .map(|b| format!("{:0>3}", b))
+          .reduce(|a, b| a + &b)
+          .unwrap();
+
+        // Split the ascii string into chars
+        let words = words.chars().collect::<Vec<_>>();
+
+        // Split into chunks of 5
+        let words = words.chunks(5);
+
+        // Run throuch each chunk and TTS samples
+        for word in words {
+          for char in word {
+            let more_samples =
+              tts.generate(&char.to_string(), self.model()).await;
+            samples.extend(more_samples);
+
+            // Short pause between letters
+            samples.extend([0.0f32; 4_000]);
+          }
+
+          // Long pause between words
+          samples.extend([0.0f32; 10_000]);
+        }
+      }
+      Some(Encoding::Phonetic) => {
+        {
+          let converter = PhoneticConverter::new(&SpellingAlphabet::Nato);
+
+          // Convert secret phrase into phonetic alphabet
+          let string = converter.convert(self.text);
+
+          // Split into words
+          let words = string.split_whitespace();
+
+          // Run throuch each word and TTS samples
+          for word in words {
+            if word.to_lowercase().as_str() == "space" {
+              // Long pause between words
+              samples.extend([0.0f32; 16_000]);
+              continue;
+            }
+
+            let more_samples = tts.generate(word, self.model()).await;
+            samples.extend(more_samples);
+
+            // Short pause between words
+            samples.extend([0.0f32; 4_000]);
+          }
+        }
+      }
+      None => {
+        let more_samples = tts.generate(self.text, self.model()).await;
+        samples.extend(more_samples);
+      }
+    }
+  }
+}
+
 #[derive(Clone, Copy, Debug)]
 enum VoiceModel {
   A,
@@ -82,135 +172,34 @@ impl Display for VoiceModel {
 
 struct Pause(u32);
 
-enum Clip<'a> {
-  /// Text to speak
-  Speak(Speak<'a>),
-
-  /// Pause in milliseconds
-  Pause(Pause),
-}
-
-impl<'a> Clip<'a> {
-  async fn render(value: Clip<'a>, samples: &mut Vec<f32>, tts: &mut Tts) {
-    match value {
-      Clip::Speak(speak) => {
-        match speak.encoding {
-          Some(Encoding::Words) => {
-            // Split the secret phrase into words
-            let words = speak.text.split_whitespace();
-
-            // Run through each word and TTS samples
-            for word in words {
-              let more_samples = tts.generate(word, speak.model()).await;
-              samples.extend(more_samples);
-            }
-          }
-          Some(Encoding::Ascii) => {
-            // Convert secret phrase into ascii codes (String of numbers)
-            let words = speak
-              .text
-              .as_bytes()
-              .iter()
-              // Convert each byte into a string, padded with 0s
-              .map(|b| format!("{:0>3}", b))
-              .reduce(|a, b| a + &b)
-              .unwrap();
-
-            // Split the ascii string into chars
-            let words = words.chars().collect::<Vec<_>>();
-
-            // Split into chunks of 5
-            let words = words.chunks(5);
-
-            // Run throuch each chunk and TTS samples
-            for word in words {
-              for char in word {
-                let more_samples =
-                  tts.generate(&char.to_string(), speak.model()).await;
-                samples.extend(more_samples);
-
-                // Short pause between letters
-                samples.extend([0.0f32; 4_000]);
-              }
-
-              // Long pause between words
-              samples.extend([0.0f32; 10_000]);
-            }
-          }
-          Some(Encoding::Phonetic) => {
-            {
-              let converter = PhoneticConverter::new(&SpellingAlphabet::Nato);
-
-              // Convert secret phrase into phonetic alphabet
-              let string = converter.convert(speak.text);
-
-              // Split into words
-              let words = string.split_whitespace();
-
-              // Run throuch each word and TTS samples
-              for word in words {
-                if word.to_lowercase().as_str() == "space" {
-                  // Long pause between words
-                  samples.extend([0.0f32; 16_000]);
-                  continue;
-                }
-
-                let more_samples = tts.generate(word, speak.model()).await;
-                samples.extend(more_samples);
-
-                // Short pause between words
-                samples.extend([0.0f32; 4_000]);
-              }
-            }
-          }
-          None => {
-            let more_samples = tts.generate(speak.text, speak.model()).await;
-            samples.extend(more_samples);
-          }
-        }
-      }
-      Clip::Pause(pause) => {
-        samples.extend(vec![0.0f32; 24 * (pause.0 as usize)])
-      }
-    };
-  }
-}
-
-impl<'a> From<Speak<'a>> for Clip<'a> {
-  fn from(speak: Speak<'a>) -> Self {
-    Clip::Speak(speak)
-  }
-}
-
-impl<'a> From<Pause> for Clip<'a> {
-  fn from(pause: Pause) -> Self {
-    Clip::Pause(pause)
+#[async_trait]
+impl Render for Pause {
+  async fn render(&self, samples: &mut Vec<f32>, _tts: &mut Tts) {
+    samples.extend(vec![0.0f32; 24 * (self.0 as usize)])
   }
 }
 
 #[tokio::main]
 async fn main() {
   let mut tts = Tts::new();
-  let clips: Vec<Clip> = vec![
-    Speak::new("This is an automated message.")
-      .with_voice(VoiceModel::A)
-      .into(),
-    Speak::new("Please listen carefully.")
-      .with_encoding(Encoding::Words)
-      .into(),
-    Pause(1_000).into(),
-    Speak::new("Hello, World!")
-      .with_encoding(Encoding::Phonetic)
-      .into(),
+  let clips: Vec<Rc<dyn Render>> = vec![
+    Rc::new(
+      Speak::new("This is an automated message.").with_voice(VoiceModel::A),
+    ),
+    Rc::new(
+      Speak::new("Please listen carefully.").with_encoding(Encoding::Words),
+    ),
+    Rc::new(Pause(1_000)),
+    Rc::new(Speak::new("Hello, World!").with_encoding(Encoding::Phonetic)),
   ];
 
   let mut samples = vec![];
 
   for clip in clips {
-    Clip::render(clip, &mut samples, &mut tts).await;
+    clip.render(&mut samples, &mut tts).await;
   }
 
-  save_audio_file(&mut samples, "output.wav");
+  save_audio_file(&mut samples, "/tmp/conet/audio.wav");
 }
 
 fn save_audio_file(samples: &mut [f32], path: &str) {
